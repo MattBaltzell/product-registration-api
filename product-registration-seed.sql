2674ee9b7f0a7ff59d0ab0d39e3b6b5ba06caa62
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2022-07-19 10:33:32 -0500

    create simple product seed file

---
+++ product-registration-seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO products
+    (sku,
+    product_name,
+    product_family,
+    product_type,
+    product_description,
+    radio_freq,
+    web_url)
+    VALUES ('test sku', 'test name', 'test family', 'test type', 'test description', 'test frequency', 'https://www.google.com')
+    
